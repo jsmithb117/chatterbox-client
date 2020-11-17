a9d321a2d9da21f14e263809fdb1c3973ec49e71
jsmithb117 <jsmithb117@gmail.com>
2020-11-16 20:59:07 -0800

    Renders a username

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -20,11 +20,22 @@ var App = {
   fetch: function(callback = ()=>{}) {
     Parse.readAll((data) => {
       // examine the response from the server request:
-      console.log(data);
-
+      console.log(data.results);
+      //html variable, empty string
+      var html = '';
+      //for loop over data.results
+      for (let i = 0; i < data.results.length; i++) {
+        //concat the results of messageView function to html
+        console.log (this)
+        // debugger;
+        html += MessageView.render({username: data.results[i].username});
+      }
+        //(jquery) append html to dom
+        $("#chats").append(html);
       callback();
     });
   },
+// }
 
   startSpinner: function() {
     App.$spinner.show();
