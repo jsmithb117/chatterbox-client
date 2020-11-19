b03cb06a6bfaa6a6ea4cd7046588e7642f94c2da
jsmithb117 <jsmithb117@gmail.com>
2020-11-18 20:52:41 -0800

    Working on getting POST request working

--- client/scripts/formView.js
+++ client/scripts/formView.js
@@ -9,8 +9,26 @@ var FormView = {
   handleSubmit: function(event) {
     // Stop the browser from submitting the form
     event.preventDefault();
-    
-    console.log('click!');
+
+    console.log(event.target.value);
+    var message = {};
+    message.text = event.originalEvent.target[0].value;
+    message.username = MessageView.filter(App.username);
+    message.roomname = Rooms.currentRoom;
+    message.createdAt = new Date();
+    var successCB = function(event) {
+      console.log (event);
+      //assign objectId to message
+      message.objectId = event.objectId;
+      //filter
+      // message = MessageView.filter(message);
+      //template it
+      // message = MessageView.render(message);
+      //render it
+      MessagesView.renderMessage(message);
+    };
+    Parse.create(message, successCB);
+    //
   },
 
   setStatus: function(active) {
@@ -19,3 +37,12 @@ var FormView = {
   }
 
 };
+
+// originalEvent.target[0].value
+
+{/* <div id= <%= objectId %> class="chat">
+<div class="username"><%= username %></div>
+<span class="message"><%= text %></span>
+<div class="roomname"><%= roomname %> </div>
+<div class="createdAt" ><%= createdAt %></div>
+</div> */}
